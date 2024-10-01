a14844fbb2c130fea5b3facf89444077840c9208
4ng3lV <est.angel.velasco@unimilitar.edu.co>
2024-10-01 15:19:45 -0500

    Add files via upload

---
+++ 2P/Carpeta Proyecto/src/parcial/Parcial.java
@@ -0,0 +1,17 @@
+
+package parcial;
+
+/**
+ *
+ * @author angel
+ */
+public class Parcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
